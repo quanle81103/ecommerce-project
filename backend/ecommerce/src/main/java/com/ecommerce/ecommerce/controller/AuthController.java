c8c8e169e0748593ab0a1d5709d43c283c70bdd5
quanle81103 <lehongquan8112003@gmail.com>
2026-06-02 00:01:28 +0700

    refactor return type

--- backend/ecommerce/src/main/java/com/ecommerce/ecommerce/controller/AuthController.java
+++ backend/ecommerce/src/main/java/com/ecommerce/ecommerce/controller/AuthController.java
@@ -2,10 +2,11 @@ package com.ecommerce.ecommerce.controller;
 
 import com.ecommerce.ecommerce.dto.AuthDto;
 import com.ecommerce.ecommerce.response.AuthResponse;
+import com.ecommerce.ecommerce.response.ResponseObject;
 import com.ecommerce.ecommerce.serviceImpl.AuthServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +17,8 @@ public class AuthController {
     private final AuthServiceImpl authService;
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthDto.LoginRequest request) {
+    public ResponseObject<AuthResponse> login(@Valid @RequestBody AuthDto.LoginRequest request) {
         AuthResponse response = authService.login(request);
-        return ResponseEntity.ok(response);
+        return new ResponseObject<>(HttpStatus.OK, "Success", response);
     }
 }
