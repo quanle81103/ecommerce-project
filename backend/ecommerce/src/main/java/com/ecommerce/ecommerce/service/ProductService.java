f7bb737e4f6a1305a7e04037e9b7970a85eb2664
quanle81103 <lehongquan8112003@gmail.com>
2026-07-01 17:22:09 +0700

    refactor: modify interface

--- backend/ecommerce/src/main/java/com/ecommerce/ecommerce/service/ProductService.java
+++ backend/ecommerce/src/main/java/com/ecommerce/ecommerce/service/ProductService.java
@@ -3,15 +3,17 @@ package com.ecommerce.ecommerce.service;
 import com.ecommerce.ecommerce.dao.Brand;
 import com.ecommerce.ecommerce.dao.Category;
 import com.ecommerce.ecommerce.dto.ProductDto;
+import jakarta.mail.Multipart;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface ProductService {
     ProductDto.ProductResponse getProductById(Long id);
 
-    ProductDto.ProductResponse addProduct(ProductDto.CreateRequest request, Long userId);
+    ProductDto.ProductResponse addProduct(ProductDto.CreateRequest request, Long userId, List<MultipartFile> files);
 
     void deleteProduct(Long productId, Long userId);
 
